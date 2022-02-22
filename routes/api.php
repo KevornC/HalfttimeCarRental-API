6e9cf3d56b5bbd769fa84f860d01251e38d29b0e
shaneika <amberstudentneika@gmail.com>
2022-02-22 07:55:06 -0500

    push

--- routes/api.php
+++ routes/api.php
@@ -18,17 +18,17 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // });
 
-Route::post('/signup/member',[App\Http\Controllers\MemberController::class,'memberSignup']);
-Route::post('/login/member',[App\Http\Controllers\LoginController::class,'memberLogin']);
+Route::post('/register',[App\Http\Controllers\MemberController::class,'memberSignup']);
+Route::post('/login',[App\Http\Controllers\LoginController::class,'memberLogin']);
 Route::post('/login/admin',[App\Http\Controllers\LoginController::class,'adminLogin']);
-Route::post('/contact',[App\Http\Controllers\ContactUsController::class,'sendMail']);
+Route::post('/contactus',[App\Http\Controllers\ContactUsController::class,'sendMail']);
 
 
 //member
 Route::get('/member/get',[App\Http\Controllers\MemberController::class,'memberGet']);
-Route::get('/member/dashboard/profile/{id}',[App\Http\Controllers\MemberController::class,'memberProfile']);
-Route::post('/member/dashboard/profile/update/{id}',[App\Http\Controllers\MemberController::class,'UpdateMemberProfile']);
-Route::get('/member/rent/order/view/{id}',[App\Http\Controllers\OrderController::class,'memberOrders']);
+Route::get('/member/profile/{id}',[App\Http\Controllers\MemberController::class,'memberProfile']);
+Route::post('/member/profile/update/{id}',[App\Http\Controllers\MemberController::class,'UpdateMemberProfile']);
+Route::get('/member/order/view/{id}',[App\Http\Controllers\OrderController::class,'memberOrders']);
 
 
 //vehicle
@@ -37,22 +37,22 @@ Route::get('/vehicle/get',[App\Http\Controllers\VehicleController::class,'vehicl
 Route::get('/vehicle/edit/get/{id}',[App\Http\Controllers\VehicleController::class,'vehicleEditGet']);
 Route::get('/vehicle/delete/{id}',[App\Http\Controllers\VehicleController::class,'vehicleDelete']);
 Route::post('/vehicle/update/{id}',[App\Http\Controllers\VehicleController::class,'vehicleUpdate']);
-Route::get('/homepage/vehicles',[App\Http\Controllers\VehicleController::class,'homepageVehicles']);
+Route::get('/home/vehicles',[App\Http\Controllers\VehicleController::class,'homepageVehicles']);
 
 
 //Order
 Route::post('/vehicle/rent/{id}',[App\Http\Controllers\OrderController::class,'vehicleRent']);
 
 //Admin
-Route::get('/rent/order/view',[App\Http\Controllers\OrderController::class,'View']);
+Route::get('/order/view',[App\Http\Controllers\OrderController::class,'View']);
 Route::get('/order/approve/{id}/{idd}',[App\Http\Controllers\OrderController::class,'approve']);
 Route::get('/order/deny/{id}/{idd}',[App\Http\Controllers\OrderController::class,'deny']);
 Route::get('/stats',[App\Http\Controllers\VehicleController::class,'Stats']);
 
 //search
 Route::post('/home/search',[App\Http\Controllers\SearchController::class,'homeSearch']);
-Route::get('/vehicle/rent/filter/',[App\Http\Controllers\SearchController::class,'rentFilterSearch']);
-Route::post('/vehicle/rent/options/',[App\Http\Controllers\SearchController::class,'options']);
-Route::get('/vehicle/rent/filter/view/{id}',[App\Http\Controllers\SearchController::class,'rentFilterSearchView']);
-Route::post('/vehicle/rent/options/',[App\Http\Controllers\SearchController::class,'options']);
+Route::get('/vehicle/filter/',[App\Http\Controllers\SearchController::class,'Filter']);
+Route::post('/vehicle/options/',[App\Http\Controllers\SearchController::class,'options']);
+Route::get('/vehicle/filter/view/{id}',[App\Http\Controllers\SearchController::class,'View']);
+Route::post('/vehicle/options/',[App\Http\Controllers\SearchController::class,'options']);
 
